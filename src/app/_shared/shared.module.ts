9035c9494a62b400d4edf0e34e7ee900457800b0
Razif Baital <benzourry@gmail.com>
2026-07-27 16:18:22 +0800

    cleanup, add icon

--- src/app/_shared/shared.module.ts
+++ src/app/_shared/shared.module.ts
@@ -16,16 +16,7 @@
 // along with LEAP.  If not, see <http://www.gnu.org/licenses/>.
 
 import { NgModule } from '@angular/core';
-// import { CommonModule } from '@angular/common';
-// import { FilterPipe } from './pipe/filter.pipe';
-// import { NgSelectModule } from '@ng-select/ng-select';
-// import { FormsModule } from '@angular/forms';
-// import { OrderByPipe } from './pipe/order-by.pipe';
-// import { PageTitleComponent } from '../_shared/component/page-title.component';
-
-// import { NgbPaginationModule, NgbModalModule, NgbNavModule, NgbDatepickerModule, NgbTooltipModule, NgbToastModule, NgbTimepickerModule,NgbAccordionModule, NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
-import { FontAwesomeModule, FaIconLibrary } from '@fortawesome/angular-fontawesome';
-// import { library } from '@fortawesome/fontawesome-svg-core';
+import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
 import {
     faCheckSquare, faSquare, 
     faTh, faPlusCircle, 
@@ -69,7 +60,10 @@ import {
     faFileImport,
     faHashtag,
     faSignature,
-    faFolder
+    faFolder, faFolder as farFolder,
+    faLink,
+    faRandom,
+    faSliders
 } from '@fortawesome/free-solid-svg-icons';
 import {
     faPlusSquare as farPlusSquare, faMinusSquare as farMinusSquare, faFlag as farFlag,
@@ -84,42 +78,12 @@ import {
 } from '@fortawesome/free-brands-svg-icons';
 import { rxStompServiceFactory } from './service/rx-stomp-service-factory';
 import { RxStompService } from './service/rx-stomp.service';
-// import { FieldEditComponent } from './component/field-edit/field-edit.component';
-// import { GroupByPipe } from './pipe/group-by.pipe';
-// import { MinDirective } from './directive/min.directive';
-// import { MaxDirective } from './directive/max.directive';
-// import { UniqueAppPathDirective } from './app-path-validator';
-// import { AppEditComponent } from './modal/app-edit/app-edit.component';
-// import { ToastsContainer } from './component/toasts-container.component';
-// import { FocusDirective } from './directive/focus.directive';
-// import { FullCalendarModule } from '@fullcalendar/angular';
-// import { SafePipe } from './pipe/safe.pipe';
-// import { MaskDirective } from './directive/mask.directive';
-// import { SecurePipe } from './pipe/secure.pipe';
-// import { AngularEditorModule } from '@kolkov/angular-editor';
-
 
 // ### DI PAKE n REFERENCE DLM main.ts
 
 @NgModule({
-    imports: [
-        // CommonModule, FormsModule, NgSelectModule,
-        // NgbPaginationModule, NgbToastModule, NgbModalModule, NgbNavModule, NgbDatepickerModule, NgbTimepickerModule, NgbTooltipModule, NgbAccordionModule, NgbDropdownModule, FontAwesomeModule, FullCalendarModule, AngularEditorModule, PageTitleComponent,
-        // FilterPipe,
-        // OrderByPipe,
-        // SecurePipe,
-        // FieldEditComponent,
-        // ToastsContainer,
-        // GroupByPipe,
-        // SafePipe,
-        // // MinDirective,
-        // // MaxDirective,
-        // MaskDirective
-    ],
-    exports: [
-        // PageTitleComponent, FilterPipe, OrderByPipe, SecurePipe, GroupByPipe, SafePipe, ToastsContainer, FieldEditComponent, CommonModule, FormsModule, NgSelectModule,
-        // NgbPaginationModule, NgbToastModule, NgbModalModule, NgbNavModule, NgbDatepickerModule, NgbTimepickerModule, NgbTooltipModule, NgbAccordionModule, NgbDropdownModule, FontAwesomeModule, FullCalendarModule, AngularEditorModule
-    ],
+    imports: [],
+    exports: [],
     providers: [{
         provide: RxStompService,
         useFactory: rxStompServiceFactory,
@@ -131,12 +95,12 @@ export class SharedModule {
         // library.addIconPacks(fas);
         library.addIcons(faExpand, faCompress, faEdit, faTasks,faChevronLeft, faPrint, faWpforms, faChevronRight, faGoogle, faFacebookF, faUniversity, faGithub, faLinkedin, faCheckSquare, faCheck, faSquare, faTh, faPlusCircle, faPencilAlt, faCog, faTrash, faPlus,faTimes,faCopy,
             faFileExcel, faFileCsv, faFilePdf, faSave, faStream, farFile, faMicrosoft, faTwitter, faQuestionCircle, faWindowRestore,faShoppingBag,faShoppingCart,faSitemap,faCommentDots, faThumbsUp,
-            farCheckSquare, farSquare, faSignOutAlt, faAngleDown, faAngleUp, faToggleOn, faToggleOff, faMapMarkedAlt,faUserEdit, faHistory, faRocket, faBox, faMailBulk, faInbox, faShareAlt, faPlay, faSync,
-            faThLarge, faEnvelope, faSearch,faCircleUser, faFilter, faAsterisk, faCaretSquareDown, faUser, faSignOutAlt, faSave, faAngleRight, faFile, faTimes, faTachometerAlt, faPlusSquare, farPlusSquare, farMinusSquare,
+            farCheckSquare, farSquare, faSignOutAlt, faAngleDown, faAngleUp, faRandom, faToggleOn, faToggleOff, faMapMarkedAlt,faUserEdit, faHistory, faRocket, faBox, faMailBulk, faInbox, faShareAlt, faPlay, faSync,
+            faThLarge, faEnvelope, faSearch,faCircleUser, faFilter, faAsterisk, faCaretSquareDown, faUser, faSignOutAlt, faSave, faAngleRight, faFile, faLink, faTimes, faTachometerAlt, faPlusSquare, farPlusSquare, farMinusSquare,
             faListOl, faCalendar, faCalendarAlt, faPaperPlane, faQuestion, faArrowUp, faArrowDown, faInfoCircle, faAngleRight, faArrowLeft, faArrowRight, faExclamationTriangle, faFileArchive,
             faReply, faQrcode, faShare, faTable, faList, farCircle, fasCircle, faFileExcel, faListAlt, faUpload, faAngleDoubleRight, faAngleDoubleLeft, faGlobe, faSort, faLock,
             faChartBar, faChartArea, faChartLine, faChartPie, faUsersCog, faUsers, faFileExport,faEllipsisH, faUncharted, faRobot, faFlag, farFlag, faImage, faBolt, faEye, faEyeSlash, faFileImport,
             faLocationCrosshairs, faLocationDot, faPlane, faMagnifyingGlassLocation, faLaptopMedical, faAddressBook, faFileInvoiceDollar, faPhotoFilm, faLayerGroup, faMessage, faDiagramProject, faFileLines,
-            faMicrophone, faClock, faHashtag, faSignature, faFolder);
+            faMicrophone, faClock, faHashtag, faSignature, faFolder, faSliders, farFolder);
     }
 }
