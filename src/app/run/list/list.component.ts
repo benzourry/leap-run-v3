99a9761e96c5da1176a92e6963dd05d8ab168262
Razif Baital <benzourry@gmail.com>
2026-09-21 09:46:14 +0800

    Fix list dataset empty result

--- src/app/run/list/list.component.ts
+++ src/app/run/list/list.component.ts
@@ -226,7 +226,7 @@ export class ListComponent implements OnInit, OnDestroy {
     this.mediaQueryListener = (e: MediaQueryListEvent) => {
       this.isMobile.set(e.matches);
     };
-    
+
     this.utilityService
       .testOnline$()
       .pipe(takeUntilDestroyed())
@@ -293,6 +293,8 @@ export class ListComponent implements OnInit, OnDestroy {
 
     this.loading.set(true);
     this.itemLoading.set(true);
+
+    this.lastResStr = '';
 
     this.entryList.set([]);
     this.selectedEntries.set({});
@@ -445,10 +447,12 @@ export class ListComponent implements OnInit, OnDestroy {
           
           if (this.lastResStr === currentResStr) {
             this.itemLoading.set(false);
+            // console.log("b4 break")
             return; // Break the infinite loop! Data is identical.
           }
           this.lastResStr = currentResStr;
           // --- FIX END ---
+    // console.log("entryList>>>:",this.dataset())
 
           const content = res.content || [];
 
